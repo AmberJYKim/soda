c09c5f4668378c5fe847b17d3d99472bef8b38a7
USER <USER@Sim>
2020-03-30 20:17:46 +0900

    03.30 20:17 심대섭 테스트용 controller method 추가

--- Omnyomnyom/src/main/java/com/soda/onn/mypage/controller/MypageController.java
+++ Omnyomnyom/src/main/java/com/soda/onn/mypage/controller/MypageController.java
@@ -1,10 +1,44 @@
 package com.soda.onn.mypage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.soda.onn.mypage.model.service.MypageService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
 
+	@Autowired
+	private MypageService mypageService;
+	
+	@GetMapping("/buyList")
+	public void buyList() {
+		
+	}
+	
+	@GetMapping("/chefRequest")
+	public void chefRequest() {
+		
+	}
+	
+	@GetMapping("/onedayList")
+	public void onedayList() {
+		
+	}
+	
+	@GetMapping("/qnaMsg")
+	public void qnaMsg() {
+		
+	}
+	
+	@GetMapping("/scrapList")
+	public void scrapList() {
+		
+	}
 }
