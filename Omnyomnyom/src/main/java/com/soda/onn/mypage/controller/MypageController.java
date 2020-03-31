0de48ed060eb04b2ba67cde311d05756db181a36
DDos-E <DDos-E@Sim>
2020-03-31 16:47:36 +0900

    03.31 16:47 심대섭 vo 수정중

--- Omnyomnyom/src/main/java/com/soda/onn/mypage/controller/MypageController.java
+++ Omnyomnyom/src/main/java/com/soda/onn/mypage/controller/MypageController.java
@@ -1,10 +1,17 @@
 package com.soda.onn.mypage.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.maven.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.soda.onn.mall.model.service.MallService;
+import com.soda.onn.mall.model.vo.BuyHistory;
 import com.soda.onn.mypage.model.service.MypageService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +23,14 @@ public class MypageController {
 
 	@Autowired
 	private MypageService mypageService;
+
+	@Autowired
+	private MallService mallService;
 	
 	@GetMapping("/buyList")
-	public void buyList() {
-		
+	public void buyList(HttpSession session) {
+		String memberId = (String) session.getAttribute("");
+		List<BuyHistory> buyList = mallService.selectBuyList(memberId);
 	}
 	
 	@GetMapping("/chefRequest")
