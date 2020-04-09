e61a9e79b17d4fca1ff6f46771423cfe08902792
DDos-E <DDos-E@Sim>
2020-04-10 02:40:55 +0900

    04.10 02:40 심대섭 자체에러픽스

--- Omnyomnyom/src/main/java/com/soda/onn/admin/controller/AdminController.java
+++ Omnyomnyom/src/main/java/com/soda/onn/admin/controller/AdminController.java
@@ -15,12 +15,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.soda.onn.admin.model.service.AdminService;
 import com.soda.onn.chef.model.service.ChefService;
 import com.soda.onn.chef.model.vo.Chef;
